fccdfd025f8fb982a408ea5d1b8d949577aefe99
oxygenplug <dan.garces2@gmail.com>
2018-05-12 11:16:15 -0400

    working on importing material design components into the workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { TimerComponent } from './timer/timer.component';
 import { OptionsMenuComponent } from './options-menu/options-menu.component';
 import { LoginMenuComponent } from './login-menu/login-menu.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,9 +14,11 @@ import { LoginMenuComponent } from './login-menu/login-menu.component';
     OptionsMenuComponent,
     LoginMenuComponent
   ],
+  // imports neccessary browser and routing modules, along with the needed material design component modules
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
