58c7320a079e107c33a1ec1feebaae9111f27999
oxygenplug <dan.garces2@gmail.com>
2018-05-14 12:55:36 -0400

    added the component decorator to the task-modal component

--- src/app/task-dashboard/task-modal/task-modal.component.ts
+++ src/app/task-dashboard/task-modal/task-modal.component.ts
@@ -1,9 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule, Input, Output, EventEmitter, Component } from "@angular/core";
 import { CommonModule } from "@angular/common"
 import { MatCardModule, MatButtonModule, MatInputModule, MatIconModule, MatProgressBarModule } from "@angular/material";
 import { TaskCardComponent } from "../task-card/task-card.component";
 import { FormsModule } from "@angular/forms";
 import { TaskCardModule } from "../task-card/task-card.module";
+import { OxyModule } from "../../oxy/oxy.module";
+
+@Component({
+  selector: "task-modal",
+  templateUrl: "./task-modal.component.html",
+  styleUrls: ["./task-modal.component.css"]
+})
+
 @NgModule({
   imports: [
     MatCardModule,
@@ -13,10 +21,13 @@ import { TaskCardModule } from "../task-card/task-card.module";
     MatIconModule,
     CommonModule,
     MatProgressBarModule,
-    TaskCardModule
+    TaskCardModule,
+    OxyModule
   ],
   exports: [TaskModalComponent],
   declarations: [TaskModalComponent],
   providers: []
 })
-export class TaskModalComponent {}
+export class TaskModalComponent {
+  
+}
